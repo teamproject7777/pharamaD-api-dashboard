9fe32f7d0e46e59878730bcea7f4db028e39808c
teamproject7777 <team.project7777@gmail.com>
2021-06-03 10:44:34 +0200

    new route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard.dashboard');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/giveaway', 'TestController@index');
+Route::get('/order', 'OrderController@index');
+Route::get('/order_detail/{id}', 'OrderController@detail');
+Route::get('/patient', 'PatientController@index');
+Route::get('/periodical', 'PeriodicalController@index');
+Route::get('/accounting', 'AccountingController@index');
+Route::get('/marketing', 'MarketingController@index');
+Route::get('/delivery', 'DeliveryController@index');
+Route::get('/delivery_detail/{id}', 'DeliveryController@show');
